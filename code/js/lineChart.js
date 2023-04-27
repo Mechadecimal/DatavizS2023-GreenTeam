bf850cb7adcbee0c9809382521b9a7603c1333bc
Joshua Martin <joshuamartintx@gmail.com>
2023-04-26 19:56:19 -0500

    Update lineChart.js
    Changed format to allow hotswapping data sets

--- code/js/lineChart.js
+++ code/js/lineChart.js
@@ -16,93 +16,79 @@
 
 
 //Modify this file to the following framework; https://d3-graph-gallery.com/graph/line_change_data.html
+//Status: done
+
+
+//Data sets should be declared before this file
 
 
 // Start of SVG
 
-    // Create svg
-    const lineChart_Svg = d3.select("#line-chart-container")
+    // Create svg, add properties and nudge to top left
+    var lineChart_Svg = d3.select("#line-chart-container")
         .append("svg")
-        .attr("width", lineChart_margin.width + lineChart_margin.left + lineChart_margin.right)
-        .attr("height", lineChart_margin.height + lineChart_margin.top + lineChart_margin.bottom)
+            .attr("width", lineChart_margin.width + lineChart_margin.left + lineChart_margin.right)
+            .attr("height", lineChart_margin.height + lineChart_margin.top + lineChart_margin.bottom)
+        .append('g')
+            .attr("transform", `translate (${lineChart_margin.left}, ${lineChart_margin.top})`)
 
-    // Create a chartGroup and nudge it to the top left
-    var lineChart_Group = lineChart_Svg.append("g")
-        .attr("transform", `translate (${lineChart_margin.left}, ${lineChart_margin.top})`)
 
-    // Read data in
+    // X axis
 
-    d3.csv("[---insert filename here---]" , function(data) {
+    var lineChart_x = d3.scaleLinear().range([0,width]);
+    var lineChart_xAxis = d3.axisBottom().scale(lineChart_x);
+        lineChart_svg.append("g")
+          .attr("transform", "translate(0," + height + ")")
+          .attr("class","lineChart_Xaxis")
 
-        //retrieve date range and 
+    // Y axis
+    var lineChart_y = d3.scaleLinear().range([height, 0]);
+    var lineChart_yAxis = d3.axisLeft().scale(lineChart_y);
+        lineChart_svg.append("g")
+            .attr("class","lineChart_Yaxis")
+
+
+
+    // Update function; call at begining and whenever data change is necessary
+    function updateStation(data, startDate, endDate) {
+
+        // update X axis:
+        lineChart_x.domain([0, d3.max(data, function(d) { return d.ser1 }) ]);
+        lineChart_svg.selectAll(".lineChart_xAxis").transition()
+            .duration(3000)
+            .call(xAxis);
         
-        // Create a list of the groups from the data
-        var lineChart_Group = ["valueA", "valueB", "valueC"];
-
-        // Add all options to the button
-        d3.select("#lineChart_YearButton")
-            .selectAll('lineChart_Options')
-     	        .data(lineChart_Group)
-            .enter()
-    	        .append('lineChart_Option')
-            .text(function (d) { return d; }) // text showed in the menu
-            .attr("value", function (d) { return d; }) // corresponding value returned by the button
-    
-        // Add X axis 
-        var x = d3.scaleLinear()
-            .domain([0,10])
-            .range([ 0, width ]);
-        lineChart_Svg.append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x));
-
-        // Add Y axis
-        var y = d3.scaleLinear()
-            .domain( [0,20])
-            .range([ height, 0 ]);
-        svg.append("g")
-            .call(d3.axisLeft(y));
-
-        // Initialize data with first group
-        var lineChart_Line = svg
-            .append('g')
-            .append("path")
-                .datum(data)
-                .attr("d", d3.line()
-                    .x(function(d) { return x(+d.time) })
-                    .y(function(d) { return y(+d.valueA) })
-                )
-                .attr("stroke", function(d){ return myColor("valueA") })
-                .style("stroke-width", 4)
-                .style("fill", "none")
-
-        // Update function
-        function lineChart_update(selectedGroup) {
-
-            // Create new data with the selection?
-            var lineChart_dataFilter = data.map(function(d){return {time: d.time, value:d[selectedGroup]} })
-
-            // New data to update line
-            line
-            .datum(lineChart_dataFilter)
+        // update Y axis
+        lineChart_y.domain([0, d3.max(data, function(d) { return d.ser2  }) ]);
+        lineChart_svg.selectAll(".lineChart_yAxis")
             .transition()
-            .duration(1000)
-            .attr("d", d3.line()
-            .x(function(d) { return x(+d.time) })
-            .y(function(d) { return y(+d.value) })
-            )
-            .attr("stroke", function(d){ return myColor(selectedGroup) })
-            }
+            .duration(3000)
+            .call(lineChart_yAxis);
+        
+        // update svg
+        var lineChart_update = lineChart_svg.selectAll(".lineTest")
+            .data([data], function(d){ return d.ser1 });
+        
+        // update line
+        lineChart_update
+            .enter()
+                .append("path")
+                    .attr("class","lineTest")
+                .merge(lineChart_update)
+                .transition()
+                    .duration(3000)
+                        .attr("d", d3.line()
+                            .x(function(d) { return x(d.ser1); })
+                            .y(function(d) { return y(d.ser2); }))
+                        .attr("fill", "none")
+                        .attr("stroke", "black")
+                        .attr("stroke-width", 3)
+        }
+        
+    // default update call
+    updateStation("[insert data to call]", "[insert startdate]", "[insert enddate]")
 
-        // When change occurs, run the update function
-        d3.select("#lineChart_YearButton").on("change", function(d) {
-            // recover the option that has been chosen
-            var selectedOption = d3.select(this).property("value")
-            // run the updateChart function with this selected option
-            update(selectedOption)
-        })
-
-        })
+    
 
         
 
