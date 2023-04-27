2bc007e1b7dd5a526bf6979da496bc8cb45734bc
Joshua Martin <joshuamartintx@gmail.com>
2023-04-27 11:32:11 -0500

    updated line chart weather update function

--- code/js/lineChart.js
+++ code/js/lineChart.js
@@ -1,18 +1,18 @@
 // Constants setup
-
+console.log("lineChart");
     // dimensions
-    var lineChart_svgWidth  = 800;
-    var lineChart_svgHeight = 600;
+    var lineChart_svgWidth  = 300;
+    var lineChart_svgHeight = 200;
 
     var lineChart_margin = {
         top: 30,
         right: 20,
         bottom: 100,
         left:30
-        },
+        };
 
-    width = lineChart_svgWidth - lineChart_margin.right - lineChart_margin.left,
-    height = lineChart_svgHeight - lineChart_margin.top - lineChart_margin.bottom;
+var lineChart_width = lineChart_svgWidth - lineChart_margin.right - lineChart_margin.left;
+var lineChart_height = lineChart_svgHeight - lineChart_margin.top - lineChart_margin.bottom;
 
 
 //Modify this file to the following framework; https://d3-graph-gallery.com/graph/line_change_data.html
@@ -27,22 +27,24 @@
     // Create svg, add properties and nudge to top left
     var lineChart_svg = d3.select("#line-chart-container")
         .append("svg")
-            .attr("width", lineChart_margin.width + lineChart_margin.left + lineChart_margin.right)
-            .attr("height", lineChart_margin.height + lineChart_margin.top + lineChart_margin.bottom)
+            .attr("width", lineChart_width + lineChart_margin.left + lineChart_margin.right)
+            .attr("height", lineChart_height + lineChart_margin.top + lineChart_margin.bottom)
         .append('g')
             .attr("transform", `translate (${lineChart_margin.left}, ${lineChart_margin.top})`)
 
 
     // X axis
 
-    var lineChart_x = d3.scaleLinear().range([0,lineChart_margin.width]);
-    var lineChart_xAxis = d3.axisBottom().scale(lineChart_x);
+    var lineChart_x = d3.scaleTime()
+        .range([0,lineChart_width]);
+    var lineChart_xAxis = d3.axisBottom()
+        .scale(lineChart_x);
         lineChart_svg.append("g")
-          .attr("transform", "translate(0," + lineChart_margin.height + ")")
+          .attr("transform", "translate(0," + lineChart_height + ")")
           .attr("class","lineChart_Xaxis")
 
     // Y axis
-    var lineChart_y = d3.scaleLinear().range([lineChart_margin.height, 0]);
+    var lineChart_y = d3.scaleLinear().range([lineChart_height, 0]);
     var lineChart_yAxis = d3.axisLeft().scale(lineChart_y);
         lineChart_svg.append("g")
             .attr("class","lineChart_Yaxis")
@@ -109,28 +111,14 @@
     //update function for weather data
     //Todo: add multiple lines
     function lineChart_updateWeather(startDate, endDate, city = 'all') {
+        console.log("updating line chart (weather)");
 
-        var lineChart_weatherData;
         //filter data by range
+        console.log("filtering data");
         d3.csv("/data/clean_data/cleaned_weather_data.csv")
         .then(function(lineChart_filterData) {
 
-            lineChart_weatherData = lineChart_filterData.filter(function(d) {
-                return endDate > d.date > startDate;
-            });
-
-            lineChart_weatherData = lineChart_weatherData.filter(function(d) {
-                if(city == 'all') {
-                    return true;
-                }
-                else {
-                    return d.city == city;
-                }
-            });
-
-            //(from /map.js)
-            //parse data variables
-            lineChart_weatherData.forEach(d => {
+            lineChart_filterData.forEach(d => {
                 // Convert string to numerical values for calculations
                 d.temperature = parseFloat(d.temperature);
                 d.humidity    = parseFloat(d.humidity);
@@ -140,15 +128,46 @@
                 d.date        = parseTime(d.date);
             });
 
+            console.log("unflitered data:");
+
+
+
+            //filter days; NEEDS TO BE TWEAKED
+
+            lineChart_filterData = lineChart_filterData.filter(function(d) {
+                console.log(d.date > startDate);
+                return d.date > startDate;
+            });
+
+            console.log("dates filtered");
+
+
+
+            //filter cities; NEEDS TO BE TWEAKED
+
+            lineChart_filterData = lineChart_filterData.filter(function(d) {
+                if(city == 'all') {
+                    return true;
+                }
+                else {
+                    return d.city == city;
+                }
+            });
+
+
+            console.log("cities filtered");
+
+            console.log(lineChart_filterData);
+
             // update X axis:
-            lineChart_x.domain([startDate, endDate]);
+            lineChart_x.domain(d3.extent(lineChart_filterData, d => d.date));
             lineChart_svg.selectAll(".lineChart_xAxis")
                 .transition()
                     .duration(3000)
                     .call(lineChart_xAxis);
             
             // update Y axis
-            lineChart_y.domain([0, d3.max(lineChart_weatherData, function(d) { return d.humidity  }) ]);
+            lineChart_y.domain([0, d3.max(lineChart_filterData, function(d) { return d.humidity  }) ]);
             lineChart_svg.selectAll(".lineChart_yAxis")
                 .transition()
                     .duration(3000)
@@ -156,7 +175,7 @@
             
             // update svg
             var lineChart_update = lineChart_svg.selectAll(".lineTest")
-                .data([lineChart_weatherData], function(d){ return d.date });
+                .data([lineChart_filterData], function(d){ return d.date });
             
             // update line
             lineChart_update
@@ -176,9 +195,9 @@
         });
     }
     
-        
+    
     // default update call (startdate, enddate)
-    lineChart_updateWeather(0, 5)
+    lineChart_updateWeather(0, 9999999)
 
     
 
